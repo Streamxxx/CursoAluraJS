b4faa61c5874ee8dbf258aca16cec019361c18c2
Streamxxx <ztiven42@gmail.com>
2024-01-22 22:23:44 -0500

    changes

--- 2024-Prog-1/app.js
+++ 2024-Prog-1/app.js
@@ -2,14 +2,15 @@ alert('¡Bienvenida y bievenido a nuestro sitio web!');
 let numeroSecreto = 4;
 let numeroUsuario = 0;
 let intentos = 1;
-palabraVeces = 'vez';
+let palabraVeces = 'vez';
+let maxIntento = 3
 //console.log(numeroUsuario);
 while(numeroSecreto != numeroUsuario){
     numeroUsuario = prompt("Me indicas numero del 1 al 10");
 
     if(numeroSecreto == numeroUsuario){
         //cumplio
-        alert(`Acertaste el numero ${numeroUsuario} Hecho en el intento ${intentos} ${palabraVeces} `);
+        alert(`Acertaste el numero ${numeroUsuario} Hecho en el intento ${intentos} ${intentos ==1 ? 'vez' : 'veces'} `);
     }else{
         if(numeroUsuario > numeroSecreto){
             alert('Numero es secreto es menor');
@@ -19,5 +20,9 @@ while(numeroSecreto != numeroUsuario){
         //incremento
         intentos++;
         palabraVeces = 'veces';
+        if(intentos > maxIntento){
+            alert(`LLegaste al numero maximo ${maxIntento} intentos`);
+            break;
+        }
     }
 }
